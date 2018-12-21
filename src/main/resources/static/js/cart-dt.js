eae3e9e68bc591b48ff30e67ee6a3de1d7fbd094
uatilman <uatilman@gmail.com>
2018-12-21 15:23:49 +0300

    Добавлена rest-security

--- src/main/resources/static/js/cart-dt.js
+++ src/main/resources/static/js/cart-dt.js
@@ -1,55 +1,58 @@
 // https://datatables.net/
 
-$(document).ready( function () {
-    var getTotalCost = function() {
+$(document).ready(function () {
+    var getTotalCost = function () {
         var res = $.ajax({
-          type: "GET",
-          url: "/geekmarket/api/cart_items/cost",
-          async: false
-        }).done(function(data) {
-          return data;
+            type: "GET",
+            url: "/geekmarket/api/cart_items/cost",
+            async: false
+        }).done(function (data) {
+            return data;
         });
         console.log(res);
         return res;
     }
 
-	 var table = $('.table').DataTable({
-	    "ajax": {
-	      "url": "/geekmarket/api/cart_items",
-	      "dataSrc": ""
-	    },
+    var table = $('.table').DataTable({
+        "ajax": {
+            "url": "/geekmarket/api/cart_items",
+            "dataSrc": ""
+        },
         "columns": [
-            { "data": "product.title" },
+            {"data": "product.title"},
             {
-              "data": null,
-              "render": function (data, type, row) {
-                return '<input type="number" class="qnt-changer" min=1 ord_product_id="' + row.product.id + '" value="' + row.quantity + '"></input>';
-              }
+                "data": null,
+                "render": function (data, type, row) {
+                    return '<input type="number" class="qnt-changer" min=1 ord_product_id="' + row.product.id + '" value="' + row.quantity + '"></input>';
+                }
             },
-            { "data": "totalPrice" }
-        ] ,
-        "initComplete": function(settings, json) {
-          $('#totalCost').text('Итоговая сумма: ' + getTotalCost().responseText);
+            {"data": "totalPrice"}
+        ],
+        "initComplete": function (settings, json) {
+            $('#totalCost').text('Итоговая сумма: ' + getTotalCost().responseText);
         }
 //        "drawCallback": function(settings) {
 //          $('#totalCost').text('Итоговая сумма: ' + getTotalCost().responseText);
 //          initAllChangers();
 //        }
-	 })
+    })
 
-    $('.table').on('click', '.qnt-changer', function() {
-       $.ajax({
-          type: "PUT",
-          url: "/geekmarket/api/cart_items",
-          async: false,
-          data: {
-            'product_id': $(this).attr('ord_product_id'),
-            'quantity': $(this).val()
-          }
-       }).done(function() {
-          table.ajax.reload();
-       });
-       $('#totalCost').text('Итоговая сумма: ' + getTotalCost().responseText);
+    $('.table').on('click', '.qnt-changer', function () {
+        $.ajax({
+            type: "PUT",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader(header, token);
+            },
+            url: "/geekmarket/api/cart_items",
+            async: false,
+            data: {
+                'product_id': $(this).attr('ord_product_id'),
+                'quantity': $(this).val()
+            }
+        }).done(function () {
+            table.ajax.reload();
+        });
+        $('#totalCost').text('Итоговая сумма: ' + getTotalCost().responseText);
     });
 
 //	 var initAllChangers = function() {
